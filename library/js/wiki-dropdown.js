6b908d643db520c24962db3ceaae30fc52f8c5fe
Nathan Edwards <nathan@dev.chand.co>
2015-05-27 16:00:23 +0100

    merged in Raj's wiki dropdown

--- library/js/wiki-dropdown.js
+++ library/js/wiki-dropdown.js
@@ -1,23 +1,24 @@
 define(['jquery'], function($) {
 
-	//if (screen.width < 968) {
+	
     // the width of browser is more then 700px
 
 
     	$(document).ready(function(){
 
+
     		//Add open class on only article clicked on and fade in close panel
-			$("article").click(function() {
-			$(this).addClass("open");
+			$("article h3").click(function() {
+			$(this).parent().toggleClass("open");
 			if ($(this).hasClass("open")) {
- 				$(this).next().fadeIn("slow");
+ 				$(this).next().fadeIn("fast");
  			}
 		});
 
 			//When close panel clicked, only close the active article
 			$(".close-sub-section").click(function(){
 				$(this).prev().removeClass("open");
-				$(this).fadeOut("slow");
+				$(this).fadeOut("medium");
   				$(this).prev().find("span").removeClass("down-arrow");
 			});
 			
