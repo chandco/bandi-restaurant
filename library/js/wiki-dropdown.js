83415d5e5e1dddda34fbaad8c4e6041c6f832ba1
NathanTest <raj2@dev.chand.co>
2015-05-28 12:17:08 +0100

    merged nathan wiki-dropdown file to raj-dev

--- library/js/wiki-dropdown.js
+++ library/js/wiki-dropdown.js
@@ -1,37 +1,9 @@
 define(['jquery', 'vendor/velocity'], function($, Velocity) {
 
-<<<<<<< HEAD
-	
-=======
->>>>>>> develop
     // the width of browser is more then 700px
     	$(document).ready(function(){
 
-
     		//Add open class on only article clicked on and fade in close panel
-<<<<<<< HEAD
-			$("article h3").click(function() {
-			$(this).parent().toggleClass("open");
-			if ($(this).parent().hasClass("open")) {
- 				$(this).parent().next().fadeIn("fast");
- 				$(this).removeClass("right-arrow");
- 				$(this).addClass("down-arrow");
- 			} else {
- 				$(this).removeClass("down-arrow");
- 				$(this).addClass("right-arrow");
- 			}
-		});
-
-			//When close panel clicked, only close the active article
-			$(".close-sub-section").click(function(){
-				$(this).prev().removeClass("open");
-				$(this).fadeOut("medium");
-  				//$(this).prev().find("span").removeClass("down-arrow");
-			});
-			
-
-			
-=======
 			$("article h2").click(function() {
 				$(this).parent().toggleClass("open");
 			});
@@ -42,6 +14,5 @@ define(['jquery', 'vendor/velocity'], function($, Velocity) {
 				$parent.removeClass("open");				
 			});
 			
->>>>>>> develop
 		});	
 });
